20c1d7c93809407a33890e63618d7e7d28411c2b
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2023-04-11 16:39:04 -0400

    Add a start-position emulator for to allow it to be used for testing networks.

--- src/BenchmarkRunner.cpp
+++ src/BenchmarkRunner.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <chrono>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wxor-used-as-pow"
+
 // Benchmarking helper class to evaluate performance of Cerebrum.
 
 using PerspectiveNetworkClippedReLU = Cerebrum::PerspectiveNetwork<
@@ -30,8 +33,45 @@ void BenchmarkEvaluate(const int samples)
     std::cout << "Evaluation output was " << output << " and took " << timeAvg << "ns!" << std::endl;
 }
 
+void EmulateBoardStartPosition()
+{
+    // Pawns
+    for (int sq = 8; sq < 16; sq++) {
+        network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(0, 0, sq);
+        network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(0, 1, sq ^ 56);
+    }
+    // Knights
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(1, 0, 1);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(1, 0, 6);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(1, 1, 1 ^ 56);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(1, 1, 6 ^ 56);
+
+    // Bishops
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(2, 0, 2);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(2, 0, 5);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(2, 1, 2 ^ 56);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(2, 1, 5 ^ 56);
+
+    // Rooks
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(3, 0, 0);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(3, 0, 7);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(3, 1, 0 ^ 56);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(3, 1, 7 ^ 56);
+
+    // Queen
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(4, 0, 3);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(4, 1, 3 ^ 56);
+
+    // King
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(5, 0, 4);
+    network.EfficientlyUpdateAccumulator<Cerebrum::AccumulatorOperation::Activate>(5, 1, 4 ^ 56);
+}
+
 int main()
 {
     network.RefreshAccumulator();
+    EmulateBoardStartPosition();
     BenchmarkEvaluate(1000000);
 }
+
+#pragma clang diagnostic pop
