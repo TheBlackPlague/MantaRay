26def4a1d0df1ba2c351e2d38604ac7c1cf508d5
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2025-04-24 00:05:15 -0400

    Define the assignment operator for accumulator.

--- include/MantaRay/Frontend/Architecture/Accumulator.h
+++ include/MantaRay/Frontend/Architecture/Accumulator.h
@@ -22,6 +22,8 @@ namespace MantaRay
 
         Accumulator(const Accumulator& src) { ArrayCopy(src.Internal, Internal); }
 
+        inline void operator =(const Accumulator& src) { ArrayCopy(src.Internal, Internal); }
+
         inline void Zero() { std::memset(Internal.data(), 0, sizeof(Array<T, N + (N * Colored)>)); }
 
         inline void Bias(Array<T, N>& bias)
