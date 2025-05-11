55c90388bf31e56f4c235ad9794a32101a51d462
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2025-05-11 08:33:23 -0400

    Better modularize CMake.

--- DownloadDependencies.cmake
+++ DownloadDependencies.cmake
@@ -1,18 +1,22 @@
 include(DownloadCPM.cmake)
 
-CPMAddPackage(
-        NAME googletest
-        GITHUB_REPOSITORY google/googletest
-        VERSION 1.16.0
-        OPTIONS
-        "INSTALL_GTEST OFF"
-        "BUILD_GMOCK OFF"
-)
+function(AddGoogleTest)
+    CPMAddPackage(
+            NAME googletest
+            GITHUB_REPOSITORY google/googletest
+            VERSION 1.16.0
+            OPTIONS
+            "INSTALL_GTEST OFF"
+            "BUILD_GMOCK OFF"
+    )
+endfunction()
 
-CPMAddPackage(
-        NAME benchmark
-        GITHUB_REPOSITORY google/benchmark
-        VERSION 1.9.3
-        OPTIONS
-        "BENCHMARK_ENABLE_TESTING OFF"
-)
+function(AddGoogleBenchmark)
+    CPMAddPackage(
+            NAME benchmark
+            GITHUB_REPOSITORY google/benchmark
+            VERSION 1.9.3
+            OPTIONS
+            "BENCHMARK_ENABLE_TESTING OFF"
+    )
+endfunction()
