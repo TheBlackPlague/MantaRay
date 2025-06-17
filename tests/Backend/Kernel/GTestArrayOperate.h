92c7ba9142c6dba3de2ed13466a8497168a994d9
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2025-06-17 09:51:16 -0400

    Improve unit tests.

--- tests/Backend/Kernel/GTestArrayOperate.h
+++ tests/Backend/Kernel/GTestArrayOperate.h
@@ -20,10 +20,17 @@ TEST(ArrayOperate, i08_512)
     HWY_ALIGN MantaRay::Array<MantaRay::i08, N> a = Generate<MantaRay::i08, N, 30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i08, N> b = Generate<MantaRay::i08, N, 04>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], static_cast<MantaRay::i08>(i % 2 == 0 ? 30 + 04 : 0));
+        EXPECT_EQ(a[i], static_cast<MantaRay::i08>(i % 2 == 0 ? 120 - 8 : 0));
 }
 
 TEST(ArrayOperate, i16_512)
@@ -33,10 +40,17 @@ TEST(ArrayOperate, i16_512)
     HWY_ALIGN MantaRay::Array<MantaRay::i16, N> a = Generate<MantaRay::i16, N, 30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i16, N> b = Generate<MantaRay::i16, N, 04>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], static_cast<MantaRay::i16>(i % 2 == 0 ? 30 + 04 : 0));
+        EXPECT_EQ(a[i], static_cast<MantaRay::i16>(i % 2 == 0 ? 120 - 8 : 0));
 }
 
 TEST(ArrayOperate, i32_512)
@@ -46,10 +60,17 @@ TEST(ArrayOperate, i32_512)
     HWY_ALIGN MantaRay::Array<MantaRay::i32, N> a = Generate<MantaRay::i32, N, 30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i32, N> b = Generate<MantaRay::i32, N, 04>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], i % 2 == 0 ? 30 + 04 : 0);
+        EXPECT_EQ(a[i], i % 2 == 0 ? 120 - 8 : 0);
 }
 
 TEST(ArrayOperate, i08_2048)
@@ -59,10 +80,17 @@ TEST(ArrayOperate, i08_2048)
     HWY_ALIGN MantaRay::Array<MantaRay::i08, N> a = Generate<MantaRay::i08, N, -30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i08, N> b = Generate<MantaRay::i08, N,  54>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], static_cast<MantaRay::i08>(i % 2 == 0 ? -30 + 54 : 0));
+        EXPECT_EQ(a[i], static_cast<MantaRay::i08>(i % 2 == 0 ? -228 : 0));
 }
 
 TEST(ArrayOperate, i16_2048)
@@ -72,10 +100,17 @@ TEST(ArrayOperate, i16_2048)
     HWY_ALIGN MantaRay::Array<MantaRay::i16, N> a = Generate<MantaRay::i16, N, -30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i16, N> b = Generate<MantaRay::i16, N,  54>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], static_cast<MantaRay::i16>(i % 2 == 0 ? -30 + 54 : 0));
+        EXPECT_EQ(a[i], static_cast<MantaRay::i16>(i % 2 == 0 ? -228 : 0));
 }
 
 TEST(ArrayOperate, i32_2048)
@@ -85,10 +120,17 @@ TEST(ArrayOperate, i32_2048)
     HWY_ALIGN MantaRay::Array<MantaRay::i32, N> a = Generate<MantaRay::i32, N, -30>();
     HWY_ALIGN MantaRay::Array<MantaRay::i32, N> b = Generate<MantaRay::i32, N,  54>();
 
-    MantaRay::ArrayOperate<MantaRay::Add>(a, b);
+    MantaRay::ArrayOperate<MantaRay::Add, MantaRay::Add, MantaRay::Add, MantaRay::Sub, MantaRay::Sub>(
+        a,
+        a,
+        a,
+        a,
+        b,
+        b
+    );
 
     for (MantaRay::usize i = 0; i < N; i++)
-        EXPECT_EQ(a[i], i % 2 == 0 ? -30 + 54 : 0);
+        EXPECT_EQ(a[i], i % 2 == 0 ? -228 : 0);
 }
 
 // ReSharper restore CppLocalVariableMayBeConst
